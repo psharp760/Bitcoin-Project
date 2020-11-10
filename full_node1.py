4f0f663cc0a6a11611b8494977bd1382fd6431a8
Peter Sharp <psharp760@gmail.com>
2020-11-10 13:28:41 -0800

    Update full_node1.py

--- full_node1.py
+++ full_node1.py
@@ -1,1 +1,10 @@
 # full_node1.py
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+serverSocket.bind(('', serverPort))
+print('full_node1 is ready to receive.')
+
+
