de7ae4a1e253f66d68abb0edda071e26da4b911b
Peter Sharp <psharp760@gmail.com>
2020-11-10 17:15:30 -0800

    updates to client and fullnode

--- clientA_send.py
+++ clientA_send.py
@@ -1,59 +1,38 @@
 # clientA_send.py
 from socket import *
 
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-def printMenu ():
-	
-	print('Client Menu\n')
+def menu():
 	print('1.	Enter new transaction\n')
 	print('2.	Display current balance of each account\n')
-	print('3.	Display unconfirmed transactions\n')
-	print('4.	Display last X number of confirmed transactions\n')
-	print('5.	Display Blockchain\n')
-	print('6.	Exit program\n')
-
-def mainMenu ():
-	
-	loop = True
-	while loop:
-		printMenu()
-		choice = input("Enter your choice [1 - 6]:\n")
-		choice = int(choice)
-
-		if (choice == 1):
-			print('Option 1 has been selected.\n')
-			transaction ()
-		elif (choice == 2):
-			print('Option 2 has been selected.\n')
-		elif (choice == 3):
-			print('Option 3 has been selected.\n')
-		elif (choice == 4):
-			print('Option 4 has been selected.\n')
-		elif (choice == 5):
-			print('Option 5 has been selected.\n')
-		elif (choice == 6):
-			print('Quitting program.\n')
-			loop = False
-		else:
-			input('ERROR: Incorrect menu option. Input any key to try again.\n')
-
-def transaction ():
-
-	print('Select the Payer:\n')
-	print('1.	A0000001\n')
-	print('2.	A0000002\n')
-	payerChoice = input('Choice:\n')
-	print('Select the Payee:\n')
-	print('1.	B0000001\n')
-	print('2.	B0000002\n')
-	payeeChoice = input('Choice:\n')
-	amountChoice = input('Input the amount of payment in decimal.\n')
-	
+	print('3. 	Display unconfirmed transactions\n')
+	print('4. 	Display last X number of confirmed transactions\n')
+	print('5. 	Display Blockchain\n')
+	print('6. 	Exit program\n')
 
 
 
-mainMenu()
+serverName = 'localhost'
+serverPort = 12345
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+clientSocket.connect((serverName, serverPort))
+
+while True:
+	menu()
+	print('Input choice: ')
+	option = input()
+	message = str.encode(str(option), 'utf-8')
+	clientSocket.send(message)
+	print(clientSocket.recvfrom(2048))
+	option = input()
+	message = str.encode(str(option), 'utf-8')
+	clientSocket.sendto(message)
+	print(str(clientSocket.recvfrom(2048)))
+
+clientSocket.close()
+
+# clientSocket.sendto(userInput.encode(), (serverName, serverPort))
+
+# modifiedInput, serverAddress = clientSocket.recvfrom(2048)
+
+# print(modifiedInput.decode())
--- full_node1.py
+++ full_node1.py
@@ -1,10 +1,36 @@
 # full_node1.py
 from socket import *
+import sys
 
-serverPort = 12000
+class Account():
+
+	def __init__(self):
+		# read her prompt, we need to have confirmed and unconfirmed balance
+		# confirmed and unconfirmed transaction, we need to read and write to files
+
+
+
+
+
+
+
+
+
+serverPort = 12345
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 serverSocket.bind(('', serverPort))
 print('full_node1 is ready to receive.')
+
+while 1: 
+	userInput, clientAddress = serverSocket.recvfrom(2048)
+	
+	modifiedInput = userInput.decode()
+	if (modifiedInput == 1):
+		print('hi')
+
+
+
+	# serverSocket.sendto(modifiedInput.encode(), clientAddress)
 
 
