fc1493f1014558502529d41f2a438c80d2a729b6
Peter Sharp <psharp760@gmail.com>
2020-11-13 01:04:18 -0800

    updated serverPort and clientPort connections.

--- full_node1.py
+++ full_node1.py
@@ -2,18 +2,15 @@
 from socket import *
 
 # socket for receiving from clientA_send.py
-serverPort1 = 12345
-serverSocket1 = socket(AF_INET, SOCK_DGRAM)
+serverPortA = 15000
+serverSocketA = socket(AF_INET, SOCK_DGRAM)
+serverSocketA.bind(('', serverPortA))
+print('full node 1 is ready to receive.\n')
 
-# socket for sending to clientA_receive.py
-serverPort2 = 54321
-serverSocket2 = socket(AF_INET, SOCK_DGRAM)
-
-serverSocket1.bind(('', serverPort1))
-print('full_node1 is ready to receive.\n')
-
-serverSocket2.bind(('', serverPort2))
-print('full_node1 is ready to send.\n')
+#sending to clientA_receive.py
+clientName = 'localhost'
+clientPortA = 15001
+clientSocketA = socket(AF_INET, SOCK_DGRAM)
 
 # open temp_T.txt to write 
 fileTxTemp = open('temp_T.txt', 'w+')
@@ -25,12 +22,12 @@ txCount = 0
 blockChain = []
 
 while 1: 
-	message, clientAddress = serverSocket1.recvfrom(2048)
+	message, clientAddress = serverSocketA.recvfrom(2048)
 	tx = message.decode()
 	# append tx to temp_T.txt
 	fileTxTemp.write(str(tx) + '\n')
-	txCount += 1
 	blockChain.append(tx)
+	txCount += 1
 
 	fileBlockChain = open('blockchain.txt', 'w')
 
@@ -38,6 +35,11 @@ while 1:
 		fullNodeBalance = ((txFee * 4)+ miningFee)
 		for line in blockChain:
 			fileBlockChain.write(''.join(line) + '')
+
+		messageToA = 'Test message to client A.\n'
+		clientSocketA.sendto(messageToA.encode(), (clientName, clientPortA))
+
+
 
 
 		# 1 2 3 4 5 6 7 8 ...
